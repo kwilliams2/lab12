68626ea10618cdc954d14c422ee45091ac11ef23
kevin williams <lee50@live.missouristate.edu>
2018-05-01 13:21:25 -0500

    Add files via upload

---
+++ bots.cpp
@@ -0,0 +1,17 @@
+//
+// Created by klw2 on 5/1/2018.
+//
+
+#include "bots.h"
+
+box
+
+bots::bots(double len, double w, double h){
+    setLength(len);
+    setWidth(w);
+    setHeight(h);
+}
+
+void bots::setLength(const double & len){
+
+}
